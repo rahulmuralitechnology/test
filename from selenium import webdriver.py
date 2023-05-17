846fa62e7a0165d5943ba1650d83f9f90fa9b631
Rahul M <95297660+rahulmuralitechnology@users.noreply.github.com>
2023-05-17 16:30:08 +0530

    Update from selenium import webdriver.py

--- from selenium import webdriver.py
+++ from selenium import webdriver.py
@@ -1,38 +1,76 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Specify the web browsers to test
-browsers = ['chrome', 'firefox', 'safari']
+# Define the target browsers and their WebDriver executables
+browsers = [
+    {"browserName": "Chrome", "executable_path": "/path/to/chromedriver"},
+    {"browserName": "Edge", "executable_path": "/path/to/msedgedriver"},
+    {"browserName": "Firefox", "executable_path": "/path/to/geckodriver"}
+    # Add more browsers here as needed
+]
 
-# Define the website URL to test
-website_url = 'http://nj-uat.qaserverix.co:3000/'
+# Define the URL of the website to be tested
+website_url = "http://nj-uat.qaserverix.co:3000/"
 
-# Loop through each browser and perform the compatibility test
+# Define the tests to be performed
+def test_button_clicks(driver):
+    try:
+        # Perform the necessary actions to test the buttons
+        driver.get(website_url)
+
+        # Find all the buttons on the page
+        buttons = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "button")))
+
+        # Click each button
+        for button in buttons:
+            button.click()
+            # Add additional assertions or validation steps here
+
+        # Print the test result
+        print(f"{driver.name} button click test passed.")
+
+    except Exception as e:
+        print(f"{driver.name} button click test failed: {str(e)}")
+
+# Perform browser compatibility testing
 for browser in browsers:
-    if browser == 'chrome':
-        driver = webdriver.Chrome()
-    elif browser == 'firefox':
-        driver = webdriver.Firefox()
-    elif browser == 'safari':
-        driver = webdriver.Safari()
+    browser_name = browser["browserName"]
+    executable_path = browser["executable_path"]
 
-    # Open the web browser and navigate to the website
-    driver.get(website_url)
+    print(f"Running button click test for {browser_name}...")
 
-    # Perform tests on the website
-    # Add your specific test scenarios here
+    try:
+        if browser_name == "Chrome":
+            options = ChromeOptions()
+            service = Service(executable_path)
+            driver = webdriver.Chrome(service=service, options=options)
+        elif browser_name == "Edge":
+            options = EdgeOptions()
+            service = EdgeService(executable_path)
+            driver = webdriver.Edge(service=service, options=options)
+        elif browser_name == "Firefox":
+            options = FirefoxOptions()
+            service = FirefoxService(executable_path)
+            driver = webdriver.Firefox(service=service, options=options)
+        else:
+            raise ValueError(f"Unsupported browser: {browser_name}")
 
-    # Example: Check if a specific element is present on the page
-    element = driver.find_element_by_id('element_id')
-    if element:
-        print(f"{browser}: Element found on the page")
-    else:
-        print(f"{browser}: Element not found on the page")
+        # Run the button click test
+        test_button_clicks(driver)
 
-    # Example: Fill out a form and submit
-    driver.find_element_by_id('name_input').send_keys('John Doe')
-    driver.find_element_by_id('email_input').send_keys('johndoe@example.com')
-    driver.find_element_by_id('submit_button').click()
+    except Exception as e:
+        print(f"Failed to run button click test for {browser_name}: {str(e)}")
 
-    # Close the web browser
-    driver.quit()
+    finally:
+        # Quit the WebDriver session
+        driver.quit()
+
+    print(f"Button click test for {browser_name} completed.\n")
